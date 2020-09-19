8465f61e130a1e205a5fcd40fc019a1d10ea9f8f
1041653026 <781153295@qq.com>
2020-09-19 17:26:29 +0800

    show组件sticky粘性定位增加能力测试，如果浏览器不支持粘性定位则更换为绝对定位， 至于饿了么组件sticky兼容是因为饿了么网页是定宽的，可以用fixed+relative

--- src/components/Show/index.vue
+++ src/components/Show/index.vue
@@ -3,11 +3,11 @@
         <main class='main'>
             <slot name='main'></slot>
         </main>
-        <div class="bottom">
+        <div :class="{bottom: true, sticky_bottom: supportSticky}">
             <section class='code' ref='code'>
                 <slot name='code'></slot>
             </section>
-            <footer :class='{footer: true, activeFooter: isShowCode}' @click='changeShow'>
+            <footer :class='{footer: true, activeFooter: isShowCode, sticky_footer: supportSticky}' @click='changeShow'>
                 <span class="triangle"></span>
                 <p class="guide">{{guide}}</p>
             </footer>
@@ -31,10 +31,20 @@ export default {
     data() {
         return {
             isShowCode: false,           // 是否显示代码
-            guide: '显示代码'            // 显示/隐藏代码
+            guide: '显示代码',            // 显示/隐藏代码
+            supportSticky: false        // 是否支持sticky粘性定位
         }
     },
+    mounted() {
+        this.testSticky();
+    },
     methods: {
+        testSticky() {
+            let body = document.body || document.documentElement;
+            body.style.position = 'sticky';
+            let _style = getComputedStyle(body) || body.currentStyle;
+            this.supportSticky = _style.position === 'sticky';
+        },
         changeShow() {
             this.isShowCode = !this.isShowCode;
             if (this.isShowCode) {
@@ -64,74 +74,85 @@ export default {
         background: rgba(255,255,255, .8);
         overflow: hidden;
     }
-    .code {
-        width: 100%;
-        height: 0;
-        overflow: hidden;
-        transition: all .3s ease-out 0s;
-        &:hover+.footer {
-            .triangle {
-                transform: translate(-3rem, -72%) rotate(180deg);
-            }
-            .guide {
-                opacity: 1;
-                transform: translate(0%, -50%);
-            }
+    .bottom {
+        position: relative;
+        padding-bottom: 5rem;
+        &.sticky_bottom {
+            padding-bottom: 0;
         }
-    }
-    .footer {
-        width: 100%;
-        height: 5rem;
-        background: rgba(255,255,255, .8);
-        transition: all .3s ease 0s;
-        cursor: pointer;
-        position: sticky;
-        bottom: 0;
-        .triangle {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -27%);
-            width: 0;
+        .code {
+            width: 100%;
             height: 0;
-            border: 0.9rem solid transparent;
-            border-top-color: #ccc;
-            transition: all .3s ease 0s;
-        }
-        .guide {
-            margin: 0;
-            padding: 0;
-            color: #ccc;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(40%, -50%);
-            opacity: 0;
-            transition: all .3s ease 0s;
-        }
-        &:hover {
-            background: rgba(243, 243, 243, 0.808);
-            .triangle {
-                border-top-color: #3A9CDB;
-                transform: translate(-3rem, -27%);
-            }
-            .guide {
-                opacity: 1 !important;
-                transform: translate(0%, -50%);
-                color: #3A9CDB;
-            }
-        }
-        &.activeFooter {
-            border-top: 1px solid #ccc;
-            .triangle {
-                transform: translate(-50%, -72%) rotate(180deg);
-            }
-            &:hover {
+            overflow: hidden;
+            transition: all .3s ease-out 0s;
+            &:hover+.footer {
                 .triangle {
                     transform: translate(-3rem, -72%) rotate(180deg);
+                }
+                .guide {
+                    opacity: 1;
+                    transform: translate(0%, -50%);
+                }
+            }
+        }
+        .footer {
+            width: 100%;
+            height: 5rem;
+            background: rgba(255,255,255, .8);
+            transition: all .3s ease 0s;
+            cursor: pointer;
+            position: absolute;
+            bottom: 0;
+            &.sticky_footer {
+                position: sticky;
+            }
+            .triangle {
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -27%);
+                width: 0;
+                height: 0;
+                border: 0.9rem solid transparent;
+                border-top-color: #ccc;
+                transition: all .3s ease 0s;
+            }
+            .guide {
+                margin: 0;
+                padding: 0;
+                color: #ccc;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(40%, -50%);
+                opacity: 0;
+                transition: all .3s ease 0s;
+            }
+            &:hover {
+                background: rgba(243, 243, 243, 0.808);
+                .triangle {
+                    border-top-color: #3A9CDB;
+                    transform: translate(-3rem, -27%);
+                }
+                .guide {
+                    opacity: 1 !important;
+                    transform: translate(0%, -50%);
+                    color: #3A9CDB;
+                }
+            }
+            &.activeFooter {
+                border-top: 1px solid #ccc;
+                .triangle {
+                    transform: translate(-50%, -72%) rotate(180deg);
+                }
+                &:hover {
+                    .triangle {
+                        transform: translate(-3rem, -72%) rotate(180deg);
+                    }
                 }
             }
         }
     }
+    
 }
 </style>
